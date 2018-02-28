670c96a9fea0b73c3799080804d143b22414d484
Marco Moroni <marcomoroni394@gmail.com>
2018-02-28 14:27:58 +0000

    Working on reading data...

--- coursework/main.cpp
+++ coursework/main.cpp
@@ -1,8 +1,21 @@
 #include <SFML\Graphics.hpp>
 #include <fstream>
+#include <iostream>
+#include <sstream>
 
 using namespace std;
 using namespace sf;
+
+struct Cavern
+{
+	Vector2i pos;
+	vector<Cavern> connections = {};
+};
+
+int noOfCaverns;
+vector<Cavern> caverns;
+Cavern* startCavern;
+Cavern* endCavern;
 
 void Reset()
 {
@@ -11,7 +24,66 @@ void Reset()
 
 void Load()
 {
+	string path = "res/input1.cav";
 
+	string buffer;
+
+	// Load in file to buffer
+	ifstream f(path);
+	if (f.good())
+	{
+		f.seekg(0, ios::end);
+		buffer.resize(f.tellg());
+		f.seekg(0);
+		f.read(&buffer[0], buffer.size());
+		f.close();
+	}
+	else
+	{
+		throw string("Couldn't open level file: ") + path;
+	}
+
+	// DEBUG
+	cout << "Buffer: " << buffer << endl;
+
+	// Get numbers
+	stringstream ss(buffer);
+	vector<int> data;
+	while (ss.good())
+	{
+		string substr;
+		getline(ss, substr, ',');
+		data.push_back(stoi(substr));
+	}
+
+	// Number of caves
+	noOfCaverns = data.at(0);
+	data.erase(data.begin());
+
+	// DEBUG
+	cout << "noOfCaverns: " << to_string(noOfCaverns) << endl;
+	for (int i = 0; i < 6; i++)
+	{
+		cout << data.at(i) << ",";
+	}
+	cout << "..." << endl;
+
+	// Coordinates
+	for (int i = 0; i < noOfCaverns; i++)
+	{
+		cout << "Looping for coord of cave " << i << "..." << endl;
+		Cavern newCavern;
+		newCavern.pos = { data.at(i), data.at(i + 1) }; // WRONG
+		data.erase(data.begin());
+		data.erase(data.begin());
+		caverns.push_back(newCavern);
+	}
+
+	// DEBUG
+	for (int i = 0; i < noOfCaverns; i++)
+	{
+		cout << "Cavern " << i << " coordinates: " << to_string(caverns.at(0).pos.x) << " " << to_string(caverns.at(0).pos.y) << endl;
+	}
 }
 
 void Update(RenderWindow &window)
